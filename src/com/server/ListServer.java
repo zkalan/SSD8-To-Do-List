aaedb5c30ff1c00bad3179a8e0ca994ca1718162
zkalan <zkalan@qq.com>
2016-12-30 18:48:00 +0800

    风格调整

--- src/com/server/ListServer.java
+++ src/com/server/ListServer.java
@@ -10,10 +10,9 @@ import javax.xml.ws.Endpoint;
 public class ListServer {
 
 	public static void main(String[] args) {
-		
+		//发布服务器的服务
 		Endpoint.publish("http://127.0.0.1:80/todolist/listservice", new ListService());
 		System.out.println("Service success!");
-		
 	}
 
 }
